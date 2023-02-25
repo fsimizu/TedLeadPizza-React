93a70a208918371b4df6b6a6eb0a3b6c6d71c094
fsimizu <fersimizu@gmail.com>
2023-02-25 18:18:26 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import NavBar from "./components/NavBar";
+
+
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+    );
+}
+
+// function Title() {
+//   return (
+//     <h1>Hola Clase</h1>
+//   )
+// }
+
+// function Description() {
+//   return (
+//     <p>Este es nuestro primer encuentro</p>
+//   )
+// }
+
+
+export default App;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,15 @@
+import './NavBar.css';
+
+function NavBar() {
+    return <div>
+        <ul>
+            <li><a href="">Menu</a></li>
+            <li><a href="">Location</a></li>
+            <li><a href="">Reviews</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>;
+}
+
+
+export default NavBar
